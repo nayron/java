f17413520d0568cb0819357b3873244b9f9ee8dd
nayron <nayron1002010@outlook.com>
2015-09-28 18:55:15 -0300

    atualização atarde 28/09/2015

--- Nayronjava/src/ICC/VetorICC.java
+++ Nayronjava/src/ICC/VetorICC.java
@@ -5,6 +5,7 @@
  */
 package ICC;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -14,26 +15,28 @@ import javax.swing.JOptionPane;
 public class VetorICC {
 
     public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
 
         int[] vetorUM = new int[3];
         int[] vetorDois = new int[3];
         int[] vetorTres = new int[6];
+        int i;
 
         int pos_Impar = 1;
         int pos_par = 2;
 
-        for (int i = 0; i <3; i++) {
-            vetorUM[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opçao " + i));
+        for ( i = 0; i <2; i++) {
+            System.out.println("Vetor 1 ");
+            vetorUM[i] = ler.nextInt();
             vetorTres[pos_Impar] = vetorUM[i];
-            pos_Impar = (pos_Impar+2);
-        }
-        for (int i = 0; i <3; i++) {
-            vetorDois[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opçao " + i));
+            pos_Impar = pos_Impar+2;
+            System.out.println("vetor 2 ");
+            vetorDois[i] = ler.nextInt();
             vetorTres[pos_par] = vetorDois[i];
-            pos_par=(pos_par+2);
+            pos_par=pos_par+2;
         }
-        
-         for(int i = 0;i<6;i++){
+      
+         for( i = 1;i<=4;i++){
              if(i % 2 == 0)
              {
                  System.err.println("vetor A  - " + vetorTres[i]);
