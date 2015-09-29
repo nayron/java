46c3aa546adc9b072c235e48889244c350115e47
adm_nayronseilert <adm_nayronseilert@mpmj231889.pmj.incra.gov.br>
2015-09-29 10:35:22 -0300

    atualização 29

--- Nayronjava/src/ICC/VetorICC.java
+++ Nayronjava/src/ICC/VetorICC.java
@@ -22,27 +22,30 @@ public class VetorICC {
         int[] vetorTres = new int[6];
         int i;
 
+        
+        int pos_par = 0;
         int pos_Impar = 1;
-        int pos_par = 2;
-
-        for ( i = 0; i <2; i++) {
-            String entrada = JOptionPane.showInputDialog("Digite um valor para vetor UM");
+        for ( i = 0; i <3; i++) {
+            String entrada = JOptionPane.showInputDialog("Digite um valor para  o vetor pos/ impar"+i);
             vetorUM[i] = Integer.parseInt(entrada);
             vetorTres[pos_Impar] = vetorUM[i];
             pos_Impar = pos_Impar+2;
-      
-            entrada = JOptionPane.showInputDialog("Digite um valor para vetor Dois");
+            
+            entrada = JOptionPane.showInputDialog("Digite um valor para o vetor pos/ par"+i);
             vetorDois[i] = Integer.parseInt(entrada);
+            
             vetorTres[pos_par] = vetorDois[i];
             pos_par=pos_par+2;
+            
         }
+       
       
-         for( i = 1;i<=4;i++){
-             if(i % 2 == 0)
+         for( i = 0;i<6;i++){
+            if(i % 2 == 0)
              {
-                 System.err.println("vetor A  - " + vetorTres[i]);
+                 System.err.println("vetor par  - " + vetorTres[i]);
              }else{
-                 System.err.println("vetor B  - " + vetorTres[i]);
+                 System.err.println("vetor impar  - " + vetorTres[i]);
              }
         }  
     }
